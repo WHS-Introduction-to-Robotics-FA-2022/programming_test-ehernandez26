93d2abe0513153d1ac4efe2da141a3e0858d73db
ehernandez26 <113933763+ehernandez26@users.noreply.github.com>
2022-12-14 11:48:29 -0700

    Create problem1.ino

---
+++ problem1.ino
@@ -0,0 +1,23 @@
+/*
+
+*/
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(10, OUTPUT);
+  
+  digitalWrite(3, HIGH);
+  digitalWrite(10, HIGH);
+  delay(1000);
+  digitalWrite(3, LOW);
+  digitalWrite(10, LOW);
+}
+
+void loop() {
+  digitalWrite(3, HIGH);
+  delay(200);
+  digitalWrite(3, LOW);
+  digitalWrite(10, HIGH);
+  delay(200);
+  digitalWrite(10, LOW);
+}
